d3958b2cc2d5cee80f392f538f1636b7f44d2e60
Stian Hanger <pdnagilum@gmail.com>
2017-08-02 16:18:04 +0200

    Refactor code and add GET request for API call /api/V1/pv

--- pvgisjsonapi/API.cs
+++ pvgisjsonapi/API.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using Float;
 using Newtonsoft.Json;
@@ -8,7 +9,87 @@ namespace pvgisjsonapi {
         /// <summary>
         /// Get PV values for given location.
         /// </summary>
-        public static PostResponse GetPVValue(FloatRouteContext ctx) {
+        public static PostResponse GetPVValueByGet(FloatRouteContext ctx) {
+            var culture = new CultureInfo("en-US");
+
+            double lat;
+            double lng;
+
+            if (!ctx.Request.Query.ContainsKey("lat") ||
+                !ctx.Request.Query.ContainsKey("lng")) {
+                throw new FloatRouteException(
+                    400,
+                    new PostResponse {
+                        message = "'lat' and 'lng' are required."
+                    });
+            }
+
+            if (!double.TryParse(ctx.Request.Query["lat"].ToString(), NumberStyles.Any, culture, out lat) ||
+                !double.TryParse(ctx.Request.Query["lng"].ToString(), NumberStyles.Any, culture, out lng)) {
+                throw new FloatRouteException(
+                    400,
+                    new PostResponse {
+                        message = "'lat' and/or 'lng' are invalid."
+                    });
+            }
+
+            double? peakpower = null;
+            double? losses = null;
+            double? slope = null;
+            double? azimuth = null;
+
+            if (ctx.Request.Query.ContainsKey("peakpower")) {
+                if (double.TryParse(ctx.Request.Query["peakpower"].ToString(), NumberStyles.Any, culture, out double temp)) {
+                    peakpower = temp;
+                }
+            }
+
+            if (ctx.Request.Query.ContainsKey("losses")) {
+                if (double.TryParse(ctx.Request.Query["losses"].ToString(), NumberStyles.Any, culture, out double temp)) {
+                    losses = temp;
+                }
+            }
+
+            if (ctx.Request.Query.ContainsKey("slope")) {
+                if (double.TryParse(ctx.Request.Query["slope"].ToString(), NumberStyles.Any, culture, out double temp)) {
+                    slope = temp;
+                }
+            }
+
+            if (ctx.Request.Query.ContainsKey("azimuth")) {
+                if (double.TryParse(ctx.Request.Query["azimuth"].ToString(), NumberStyles.Any, culture, out double temp)) {
+                    azimuth = temp;
+                }
+            }
+
+            var data = compileRequest(
+                lat,
+                lng,
+                ctx.Request.Query.ContainsKey("pvtech") ? ctx.Request.Query["pvtech"].ToString() : null,
+                peakpower,
+                losses,
+                ctx.Request.Query.ContainsKey("mounting") ? ctx.Request.Query["mounting"].ToString() : null,
+                slope,
+                azimuth);
+
+            if (data != null) {
+                return new PostResponse {
+                    message = "Ok",
+                    data = data
+                };
+            }
+
+            throw new FloatRouteException(
+                500,
+                new PostResponse {
+                    message = "Unable to query PVGIS for info. Try again later."
+                });
+        }
+
+        /// <summary>
+        /// Get PV values for given location.
+        /// </summary>
+        public static PostResponse GetPVValueByPost(FloatRouteContext ctx) {
             PostValues pv;
 
             try {
@@ -22,8 +103,71 @@ namespace pvgisjsonapi {
                     });
             }
 
+            var data = compileRequest(
+                pv.lat,
+                pv.lng,
+                pv.pvtech,
+                pv.peakpower,
+                pv.losses,
+                pv.mounting,
+                pv.slope,
+                pv.azimuth);
+
+            if (data != null) {
+                return new PostResponse {
+                    message = "Ok",
+                    data = data
+                };
+            }
+
+            throw new FloatRouteException(
+                500,
+                new PostResponse {
+                    message = "Unable to query PVGIS for info. Try again later."
+                });
+        }
+
+        /// <summary>
+        /// Serve the front page.
+        /// </summary>
+        public static FloatRouteResponse ServeFrontpage(FloatRouteContext ctx) {
+            var html = Cacher.Get("Frontpage", () => {
+                var file = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "index.html");
+
+                return File.Exists(file)
+                    ? File.ReadAllText(file)
+                    : null;
+            });
+
+            if (string.IsNullOrWhiteSpace(html)) {
+                throw new FloatRouteException(404);
+            }
+
+            return new FloatRouteHtmlResponse(html);
+        }
+
+        #region Helper functions
+
+        /// <summary>
+        /// Compile the values and get from cache or website.
+        /// </summary>
+        private static PVGIS.Response compileRequest(double lat, double lng, string pvtech, double? peakpower, double? losses, string mounting, double? slope, double? azimuth) {
+            var pv = new PostValues {
+                lat = lat,
+                lng = lng,
+                pvtech = pvtech,
+                peakpower = peakpower,
+                losses = losses,
+                mounting = mounting,
+                slope = slope,
+                azimuth = azimuth
+            };
+
             var key = JsonConvert.SerializeObject(pv);
-            var data = Cacher.Get(key, () => {
+
+            return Cacher.Get(key, () => {
                 var options = new PVGIS.Options();
 
                 // PVTechnology
@@ -80,43 +224,11 @@ namespace pvgisjsonapi {
                     return null;
                 }
             });
-
-            if (data != null) {
-                return new PostResponse {
-                    message = "Ok",
-                    data = data
-                };
-            }
-
-            throw new FloatRouteException(
-                500,
-                new PostResponse {
-                    message = "Unable to query PVGIS for info. Try again later."
-                });
         }
 
-        /// <summary>
-        /// Serve the front page.
-        /// </summary>
-        public static FloatRouteResponse ServeFrontpage(FloatRouteContext ctx) {
-            var html = Cacher.Get("Frontpage", () => {
-                var file = Path.Combine(
-                    Directory.GetCurrentDirectory(),
-                    "index.html");
+        #endregion
 
-                return File.Exists(file)
-                    ? File.ReadAllText(file)
-                    : null;
-            });
-
-            if (string.IsNullOrWhiteSpace(html)) {
-                throw new FloatRouteException(404);
-            }
-
-            return new FloatRouteHtmlResponse(html);
-        }
-
-        #region Helper Classes
+        #region Helper classes
 
         public class PostResponse {
             public string message { get; set; }
--- pvgisjsonapi/Program.cs
+++ pvgisjsonapi/Program.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.Hosting;
 namespace pvgisjsonapi {
     public class Program {
         public static void Main(string[] args) {
+            // Serve frontpage.
             FloatEngine.RegisterRouteFunction("", FloatHttpMethod.GET, API.ServeFrontpage);
 
             // Get PV values for given location.
-            FloatEngine.RegisterRouteFunction("api/v1/pv", FloatHttpMethod.POST, API.GetPVValue);
+            FloatEngine.RegisterRouteFunction("api/v1/pv", FloatHttpMethod.GET, API.GetPVValueByGet);
+            FloatEngine.RegisterRouteFunction("api/v1/pv", FloatHttpMethod.POST, API.GetPVValueByPost);
 
             var host = new WebHostBuilder()
                 .UseKestrel()
